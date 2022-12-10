a7fbcd17b3b199f7824953b20ab53f503ca08c3d
Korotkov Dmitry <2110821@mail.ru>
2022-12-10 23:09:12 +0300

    feat(src): Add 3 streams with different operations

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        // 1. Найти количество несовершеннолетних (т.е. людей младше 18 лет).
+        long countOfUnderAgePersons = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println("Количество несовершеннолетних: " + countOfUnderAgePersons);
+
+        // 2. Получить список фамилий призывников (т.е. мужчин от 18 и до 27 лет).
+        List<String> conscriptSurnames = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN)
+                .filter(person -> person.getAge() >= 18 && person.getAge() <= 27)
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+
+        // 3. Получить отсортированный по фамилии список потенциально работоспособных людей с высшим образованием в выборке
+        // (т.е. людей с высшим образованием от 18 до 60 лет для женщин и до 65 лет для мужчин).
+        List<Person> workablePersons = persons.stream()
+                .filter(person -> person.getAge() >= 18 && person.getAge() <= 65)
+                .filter(person -> person.getSex() == Sex.MAN || (person.getSex() == Sex.WOMAN && person.getAge() <= 60))
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
+    }
+}
